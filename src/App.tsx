3fb6cc4af0ac8bd038c7c9a7ef2603c379a365f4
Filipe <filipesilveira.dev@gmail.com>
2026-06-17 19:04:22 -0300

    variáveis de cores customizadas criadas e importadas no index.css

--- src/App.tsx
+++ src/App.tsx
@@ -1,9 +1,9 @@
 
 function App() {
   return (
-    <div className="flex h-screen items-center justify-center bg-slate-900">
-      <h1 className="text-4xl font-bold text-emerald-400 drop-shadow-md">
-        Template Prontinho! 🚀
+    <div>
+      <h1 >
+        Hello, world!
       </h1>
     </div>
   );
