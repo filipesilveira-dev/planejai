a08de4f68f6f48511162aa172a759bd94c97e208
Filipe <filipesilveira.dev@gmail.com>
2026-07-05 13:41:37 -0300

    botão de 'ver detalhe' ok

--- src/pages/SimulationHistoryPage.tsx
+++ src/pages/SimulationHistoryPage.tsx
@@ -79,6 +79,7 @@ export function SimulationHistoryPage() {
                 Excluir
               </Button>
               <Button
+              onClick={()=>{void navigate(`/resultado/${sim.id}`)}}
                 variant="primary"
                 className="bg-primary text-white px-4 py-2 rounded-lg text-sm"
               >
